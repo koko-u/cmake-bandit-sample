48655058319f229202fc5731c2aa4091b8e39a67
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2014-04-03 23:22:22 +0900

    add doxygen file

--- include/fraction.h
+++ include/fraction.h
@@ -3,12 +3,29 @@
 
 #include <ostream>
 
+/**
+ * @class 分数クラス
+ * @brief 分数を表現するサンプルクラス
+ *
+ * あくまでサンプルなので、大した実装はない
+ */
 class Fraction {
     friend std::ostream& operator<<(std::ostream& os, Fraction const& f);
     friend Fraction operator+(Fraction const& lhs, Fraction const& rhs);
     friend bool operator==(Fraction const& lhs, Fraction const& rhs);
 public:
+    /** デフォルトコンストラクタ
+     *
+     *  デフォルトでは０とする
+     */
     Fraction() : Fraction(0, 1) {}
+    /** コンストラクタ
+     *
+     * @param[in] num   分子
+     * @param[in] denom 分母
+     *
+     * @exception 分母に 0 を指定すると ZeroDivisionError が投げられる
+     */
     Fraction(int num, int denom);
 
 private:
--- include/mathlib.h
+++ include/mathlib.h
@@ -1,8 +1,18 @@
 #ifndef MATHLIB_H
 #define MATHLIB_H
 
+/**
+ * @namespace Mathlib
+ */
 namespace Mathlib {
 
+/**
+ * @brief 最大公約数を計算する
+ *
+ * @param[in] n 整数
+ * @param[in] m 整数
+ * @return    引数に指定した二数の最大公約数
+ */
 int gcd(int n, int m);
 
 }
